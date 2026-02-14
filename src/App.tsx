4ee98c69bccf16f86aff86b7fad0aa7bcb537e8e
maestrorafa05 <130625704+maestrorafa05@users.noreply.github.com>
2026-02-14 15:13:52 +0700

    #1 Todos
    - Build Sidebar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
 import './App.css'
 
-let name:string;
-
-name = "Maestro"
+const title = 'PARAS'
 
 function App() {
   return (
-    <>
-    <h1>hallo</h1>  
-    </>
+    <div className="app">
+      <aside className="sidebar">
+        <h1 className="title">{title}</h1>
+        <ul className="menu">
+          <li><a href="#">Beranda</a></li>
+          <li><a href="#">Peminjaman Ruangan</a></li>
+          <li><a href="#">Daftar Ruangan</a></li>
+        </ul>
+        <footer className="sidebar-footer">
+          <p>Copyright 2026 @ maestrorafa05</p>
+        </footer>
+      </aside>
+    </div>
   )
 }
 
